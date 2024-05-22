f56efd523ace4b0964df843c7aed5b2aa78d2a6c
omchien <chien.ong@ecaraid.com>
2024-05-22 17:44:58 +0700

    render config key

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,22 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  return <div className="App">Dev</div>;
+  const configKeys = [
+    { key: 'REACT_APP_API_KEY', value: process.env.REACT_APP_API_KEY },
+    { key: 'REACT_APP_AUTH_DOMAIN', value: process.env.REACT_APP_AUTH_DOMAIN },
+    { key: 'REACT_APP_PROJECT_ID', value: process.env.REACT_APP_PROJECT_ID },
+    { key: 'REACT_APP_STORAGE_BUCKET', value: process.env.REACT_APP_STORAGE_BUCKET },
+    { key: 'REACT_APP_MESSAGING_SENDER_ID', value: process.env.REACT_APP_MESSAGING_SENDER_ID },
+    { key: 'REACT_APP_APP_ID', value: process.env.REACT_APP_APP_ID },
+    { key: 'REACT_APP_MEASUREMENT_ID', value: process.env.REACT_APP_MEASUREMENT_ID },
+    { key: 'REACT_APP_DATABASE_URL', value: process.env.REACT_APP_DATABASE_URL },
+  ];
+  return (
+    <div className="App">
+      Config keys:
+      {configKeys.map((item) => `${item.key}: ${item.value}`)}
+    </div>
+  );
 }
 
 export default App;
